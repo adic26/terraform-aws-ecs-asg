7d74fe2d1d2899e0a6b446a20c2887f9de3ab2ff
Aditya Chugh <chugh.aditya@gmail.com>
2018-10-26 12:13:09 -0400

    Adding OPSVR Workers

--- ecs.tf
+++ ecs.tf
@@ -155,30 +155,7 @@ resource "random_id" "code" {
 # EOF
 # }
 
-/*
-*  Create ECS Task Definition - Backend Services
-*/
-# Simply specify the family to find the latest ACTIVE revision in that family.
-data "template_file" "opsvrweb" {
-  template = "${file("${path.module}/templates/opsvr_ecs_task.tpl")}"
 
-  vars {
-    opsvr_app_key = "${var.opsvr_app_key}"
-    mailtrap_password = "${var.mailtrap_password}"
-    mailtrap_username = "${var.mailtrap_username}"
-    mongo_audit_dsn = "${var.mongo_audit_dsn}"
-    mongo_dsn = "${var.mongo_dsn}"
-    opsvr_image = "${var.opsvr_image}"
-  }
-}
-data "aws_ecs_task_definition" "opsvrweb" {
-  task_definition = "${aws_ecs_task_definition.opsvrweb.family}"
-}
-
-resource "aws_ecs_task_definition" "opsvrweb" {
-  family = "opsvr-${var.cluster_name}-web"
-  container_definitions = "${data.template_file.opsvrweb.rendered}"
-}
 
 # Create a new load balancer
 resource "aws_elb" "opsvr" {
@@ -200,24 +177,4 @@ resource "aws_elb" "opsvr" {
   tags {
     Name = "elb-${var.cluster_name}"
   }
-}
-
-
-resource "aws_ecs_service" "opsvrweb" {
-  name          = "opsvr-${var.cluster_name}-web"
-  cluster       = "${aws_ecs_cluster.ecs_cluster.id}"
-  desired_count = 2
-
-  # Track the latest ACTIVE revision
-  task_definition = "${aws_ecs_task_definition.opsvrweb.family}:${max("${aws_ecs_task_definition.opsvrweb.revision}", "${data.aws_ecs_task_definition.opsvrweb.revision}")}"
-
-  load_balancer {
-    elb_name = "opsvr-${var.cluster_name}-elb"
-    container_name = "web"
-    container_port = "8088"
-  }
-
-  depends_on = [
-    "aws_elb.opsvr"
-  ]
 }
---
+++ opsvr_workers.tf
@@ -0,0 +1,59 @@
+module "allocation" {
+  source = "../module.terraform-opsvr-workers"
+  cluster_name = "${var.cluster_name}"
+  cluster_id = "${aws_ecs_cluster.ecs_cluster.id}"
+  fam_worker_type = "worker-general-allocation"
+  queue_worker_type = "opsvr-${var.cluster_name}-allocation"
+  worker_type = "allocation"
+  opsvr_app_key = "${var.opsvr_app_key}"
+  mailtrap_password = "${var.mailtrap_password}"
+  mailtrap_username = "${var.mailtrap_username}"
+  mongo_audit_dsn = "${var.mongo_audit_dsn}"
+  mongo_dsn = "${var.mongo_dsn}"
+  opsvr_image = "${var.opsvr_image}"
+}
+
+module "autodel" {
+  source = "../module.terraform-opsvr-workers"
+  cluster_name = "${var.cluster_name}"
+  cluster_id = "${aws_ecs_cluster.ecs_cluster.id}"
+  fam_worker_type = "worker-autodel"
+  queue_worker_type = "opsvr-${var.cluster_name}-autodelivery"
+  worker_type = "autodel"
+  opsvr_app_key = "${var.opsvr_app_key}"
+  mailtrap_password = "${var.mailtrap_password}"
+  mailtrap_username = "${var.mailtrap_username}"
+  mongo_audit_dsn = "${var.mongo_audit_dsn}"
+  mongo_dsn = "${var.mongo_dsn}"
+  opsvr_image = "${var.opsvr_image}"
+}
+
+module "high" {
+  source = "../module.terraform-opsvr-workers"
+  cluster_name = "${var.cluster_name}"
+  cluster_id = "${aws_ecs_cluster.ecs_cluster.id}"
+  queue_worker_type = "opsvr-${var.cluster_name}-high-priority"
+  fam_worker_type = "worker-general-high"
+  worker_type = "high"
+  opsvr_app_key = "${var.opsvr_app_key}"
+  mailtrap_password = "${var.mailtrap_password}"
+  mailtrap_username = "${var.mailtrap_username}"
+  mongo_audit_dsn = "${var.mongo_audit_dsn}"
+  mongo_dsn = "${var.mongo_dsn}"
+  opsvr_image = "${var.opsvr_image}"
+}
+
+module "med-low" {
+  source = "../module.terraform-opsvr-workers"
+  cluster_name = "${var.cluster_name}"
+  cluster_id = "${aws_ecs_cluster.ecs_cluster.id}"
+  queue_worker_type = "opsvr-${var.cluster_name}-med-priority,opsvr-${var.cluster_name}-low-priority"
+  fam_worker_type = "worker-general-med-low"
+  worker_type = "med-low"
+  opsvr_app_key = "${var.opsvr_app_key}"
+  mailtrap_password = "${var.mailtrap_password}"
+  mailtrap_username = "${var.mailtrap_username}"
+  mongo_audit_dsn = "${var.mongo_audit_dsn}"
+  mongo_dsn = "${var.mongo_dsn}"
+  opsvr_image = "${var.opsvr_image}"
+}
