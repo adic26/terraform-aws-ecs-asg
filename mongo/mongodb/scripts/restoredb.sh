62b6cb4acf908cc2094ff7d5f0c8f737f4eba07e
Aditya Chugh <chugh.aditya@gmail.com>
2018-11-28 00:18:43 -0500

    Add all collections except Deliveries

--- mongo/mongodb/scripts/restoredb.sh
+++ mongo/mongodb/scripts/restoredb.sh
@@ -4,6 +4,7 @@ set -e
 MONGO_DB_NAME="sit-opsserver"
 TIMESTAMP=$(date +"%Y-%m-%d")
 MONGO_CLUSTER="$1/$2:27017,$3:27017,$4:27017"
+#mongodb://db1.example.net:27017,db2.example.net:2500/?replicaSet=test
 
 
 # command to download the latest production snapshot
@@ -20,7 +21,7 @@ fi
 
 #Get app keys and apicollection
 aws s3 sync s3://chefsplate-devops/apiKeysCollection/Staging/ ./
-restoretosit=$(mongorestore --verbose --host $MONGO_CLUSTER -d $MONGO_DB_NAME  --nsInclude 'opsserver-main.*' --nsExclude 'opsserver-main.app' --nsExclude 'opsserver-main.apikeys' --nsExclude 'opsserver-main.deliveries' --nsExclude 'opsserver-main.customers' --nsFrom='opsserver-main.*' --nsTo='sit-opsserver.*' --drop --maintainInsertionOrder --gzip --archive=$BACKUPFILE)
+restoretosit=$(mongorestore --verbose --host $MONGO_CLUSTER -d $MONGO_DB_NAME  --nsInclude 'opsserver-main.*' --nsExclude 'opsserver-main.app' --nsExclude 'opsserver-main.apikeys' --nsExclude 'opsserver-main.deliveries' --nsFrom='opsserver-main.*' --nsTo='sit-opsserver.*' --drop --maintainInsertionOrder --gzip --archive=$BACKUPFILE)
 mongoimport --host $MONGO_CLUSTER -d $MONGO_DB_NAME --collection apikeys --file apikeys.json
 mongoimport --host $MONGO_CLUSTER -d $MONGO_DB_NAME --collection app --file app.json
 
